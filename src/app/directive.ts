8843f4fc7715294da83bebea33fe23959f71270e
Akhilreddy511 <git@stackblitz.com>
2025-08-05 18:25:52 -0500

    1 file added

---
+++ src/app/directive.ts
@@ -0,0 +1,48 @@
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+@Directive({
+  selector: '[ssnMask]',
+  standalone: true,
+})
+export class SSNMaskDirective {
+  @Input('ssnMask') enabled: boolean = true;
+  @Input() show: boolean = false;
+
+  private actualValue: string = '';
+
+  constructor(private el: ElementRef<HTMLInputElement>) {}
+
+  @HostListener('input', ['$event'])
+  onInput(event: Event) {
+    const input = (event.target as HTMLInputElement).value.replace(/\D/g, '');
+    this.actualValue = input.slice(0, 9); // Max 9 digits
+    this.updateView();
+  }
+
+  private formatSSN(value: string): string {
+    const part1 = value.slice(0, 3);
+    const part2 = value.slice(3, 5);
+    const part3 = value.slice(5, 9);
+    return `${part1}${part2 ? '-' + part2 : ''}${part3 ? '-' + part3 : ''}`;
+  }
+
+  private maskSSN(value: string): string {
+    return value.replace(/\d/g, 'X');
+  }
+
+  private updateView() {
+    const formatted = this.formatSSN(this.actualValue);
+    this.el.nativeElement.value =
+      this.enabled && !this.show ? this.maskSSN(formatted) : formatted;
+  }
+
+  // Called by external components
+  toggleShow(show: boolean) {
+    this.show = show;
+    this.updateView();
+  }
+
+  getValue() {
+    return this.actualValue;
+  }
+}
