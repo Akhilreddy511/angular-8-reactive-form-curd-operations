33a776238ec11232b8af4882abae028fdb02b96f
Akhilreddy511 <git@stackblitz.com>
2025-08-05 19:41:13 -0500

    1 file modified

--- src/app/directive.ts
+++ src/app/directive.ts
@@ -1,11 +1,11 @@
 import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
-  selector: '[ssnMask]',
+  selector: '[maskPattern]',
   standalone: true,
 })
-export class SSNMaskDirective {
-  @Input('ssnMask') enabled: boolean = true;
+export class MaskDirective {
+  @Input('maskPattern') enabled: boolean = true;
   @Input() show: boolean = false;
 
   private actualValue: string = '';
@@ -13,30 +13,39 @@ export class SSNMaskDirective {
   constructor(private el: ElementRef<HTMLInputElement>) {}
 
   @HostListener('input', ['$event'])
-  onInput(event: Event) {
-    const input = (event.target as HTMLInputElement).value.replace(/\D/g, '');
-    this.actualValue = input.slice(0, 9); // Max 9 digits
+  onInput(event: InputEvent) {
+    const raw = this.el.nativeElement.value.replace(/\D/g, '');
+    this.actualValue = raw.slice(0, 9); // only 9 digits allowed
     this.updateView();
   }
 
-  private formatSSN(value: string): string {
+  @HostListener('keydown', ['$event'])
+  onKeyDown(event: KeyboardEvent) {
+    if (!this.show && event.key === 'Backspace') {
+      event.preventDefault(); // don't delete X
+      this.actualValue = this.actualValue.slice(0, -1); // remove last digit
+      this.updateView();
+    }
+  }
+
+  private format(value: string): string {
     const part1 = value.slice(0, 3);
     const part2 = value.slice(3, 5);
     const part3 = value.slice(5, 9);
-    return `${part1}${part2 ? '-' + part2 : ''}${part3 ? '-' + part3 : ''}`;
+    return [part1, part2, part3].filter(Boolean).join('-');
   }
 
-  private maskSSN(value: string): string {
-    return value.replace(/\d/g, 'X');
+  private mask(value: string): string {
+    const masked = 'X'.repeat(value.length);
+    return this.format(masked);
   }
 
   private updateView() {
-    const formatted = this.formatSSN(this.actualValue);
+    const formatted = this.format(this.actualValue);
     this.el.nativeElement.value =
-      this.enabled && !this.show ? this.maskSSN(formatted) : formatted;
+      this.enabled && !this.show ? this.mask(this.actualValue) : formatted;
   }
 
-  // Called by external components
   toggleShow(show: boolean) {
     this.show = show;
     this.updateView();
@@ -47,30 +56,20 @@ export class SSNMaskDirective {
   }
 }
 
-// @Input() ssnMask: boolean = false;
-// @ViewChild(SSNMaskDirective) ssnDirective?: SSNMaskDirective;
+// @Input() maskPattern: boolean = false;
+// @ViewChild(MaskDirective) maskDirective?: MaskDirective;
 
 // toggleShow() {
 //   this.show = !this.show;
-//   this.ssnDirective?.toggleShow(this.show);
+//   this.maskDirective?.toggleShow(this.show);
 // }
 
-// <input
-//   class="form-control"
-//   [type]="'text'"
-//   (input)="onInput($event)"
-//   [ssnMask]="ssnMask"
-//   [show]="show"
-// />
-// <a *ngIf="ssnMask" class="toggle-link" (click)="toggleShow()">
-//   {{ show ? 'Hide' : 'Show' }}
-// </a>
-
 // <app-input-field
+//   class="ssnCss"
 //   label="Social Security Number"
+//   hint="Enter only last 4 digits"
 //   formControlName="ssn"
-//   [mask]="true"
-//   [ssnMask]="true"
-//   [numbersOnly]="true"
+//   [maskPattern]="true"
+//   [maxLength]="9"
 //   ...
 // ></app-input-field>
